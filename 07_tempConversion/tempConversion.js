6205e475ccb32e54a74fbc6c27aef144cb8e1d41
Sandra <sandramai432@gmail.com>
2023-02-27 17:16:22 -0500

    tempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,23 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(temp) {
+  if (temp == 32) {
+    return 0;
+  }
+
+  let result = (temp - 32) * (5/9);
+  result = Math.round(result * 10) / 10;
+
+  return parseFloat(result.toFixed(1));
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(temp) {
+  if (temp == 0) {
+    return 32;
+  }
+
+  let result = temp * (9/5) + 32;
+  result = Math.round(result * 10) / 10;
+
+  return parseFloat(result.toFixed(1));
 };
 
 // Do not edit below this line
