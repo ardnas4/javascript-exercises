ba9fac1370c7ab6ff00edd7ee81d661eb818ac08
Sandra <sandramai432@gmail.com>
2023-02-27 16:13:02 -0500

    completed removeFromArray

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,13 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, ...args) {
+    let result = [];
 
+    array.forEach((element) => {
+        if (args.includes(element) == false) {
+            result.push(element);
+        }
+    });
+
+    return result;
 };
 
 // Do not edit below this line
