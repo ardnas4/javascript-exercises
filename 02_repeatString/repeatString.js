64f304ad19466f7f2ae044c4f21abd5523585cc5
Sandra <sandramai432@gmail.com>
2023-02-27 15:15:53 -0500

    completed repeatString

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,15 @@
-const repeatString = function() {
+const repeatString = function(string, num) {
+    var resultStr = "";
 
+    if (num < 0) {
+        return "ERROR";
+    }
+    
+    for (let i = 0; i < num; i++) {
+        resultStr += string;
+    }
+
+    return resultStr;
 };
 
 // Do not edit below this line
