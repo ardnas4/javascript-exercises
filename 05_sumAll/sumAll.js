30cc571a287efca67a764ef5e0159bc4547ec6c5
Sandra <sandramai432@gmail.com>
2023-02-27 16:31:39 -0500

    completed sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,26 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if ((a < 0 || b < 0) || (typeof a !== "number") || (typeof b !== "number")) {
+        return "ERROR";
+    }
 
+    let sum = 0;
+    let start = 0, end = 0;
+
+    if (a > b) {
+        start = b;
+        end = a;
+    }
+
+    else {
+        start = a;
+        end = b;
+    }
+
+    for (let i = start; i <= end; i++) {
+        sum += i;
+    }
+
+    return sum;
 };
 
 // Do not edit below this line
